3815bba718831e8a4d699d87d7ce84fc3492a61f
Will F <11763006+w4ester@users.noreply.github.com>
2021-01-19 20:00:00 -0500

    add candle module -- create, light, extinguish state management

---
+++ candle/candle.py
@@ -0,0 +1,32 @@
+"""
+Candle state management.
+
+A candle has a lifecycle: created -> lit -> expired.
+"""
+
+import time
+import uuid
+
+
+def create_candle(vigil_id, ip_address, burn_hours=24):
+    """Create a new candle in a vigil."""
+    candle = {
+        "id": str(uuid.uuid4())[:8],
+        "vigil_id": vigil_id,
+        "lit_at": time.time(),
+        "expires_at": time.time() + (burn_hours * 3600),
+        "ip_address": ip_address,
+        "active": True,
+    }
+    return candle
+
+
+def is_expired(candle):
+    """Check if a candle has burned out."""
+    return time.time() > candle["expires_at"]
+
+
+def extinguish(candle):
+    """Mark a candle as no longer active."""
+    candle["active"] = False
+    return candle
