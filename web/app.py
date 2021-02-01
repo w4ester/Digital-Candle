a4a0a0fc967b6ad6e4d4db0e93dfd95962bb02ee
Will F <11763006+w4ester@users.noreply.github.com>
2021-01-31 20:00:00 -0500

    wire flask routes to candle module -- light a candle from the browser

--- web/app.py
+++ web/app.py
@@ -5,7 +5,15 @@ Flask server for digital vigil candles.
 """
 
 import argparse
-from flask import Flask, render_template
+import sys
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+from candle.candle import create_candle
+from candle.store_sqlite import init_db, save_candle, get_active_candles, list_vigils
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "digital-candle-secret"
@@ -14,7 +22,17 @@ app.config["SECRET_KEY"] = "digital-candle-secret"
 @app.route("/")
 def index():
     """Show the main page."""
-    return "<h1>Digital-Candle</h1><p>Coming soon.</p>"
+    candles = get_active_candles("default")
+    return render_template("index.html", candles=candles)
+
+
+@app.route("/light", methods=["POST"])
+def light():
+    """Light a new candle."""
+    ip = request.remote_addr or "unknown"
+    candle = create_candle("default", ip)
+    save_candle(candle)
+    return redirect(url_for("index"))
 
 
 def parse_args():
@@ -42,4 +60,5 @@ if __name__ == "__main__":
     print(f"  Debug:  {args.debug}")
     print("=" * 50)
 
+    init_db()
     app.run(host="0.0.0.0", port=args.port, debug=args.debug)
