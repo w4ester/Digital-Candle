17a8bc3738ac79372c01421ad6a5f65556efe45f
Will F <11763006+w4ester@users.noreply.github.com>
2021-02-18 20:00:00 -0500

    add vigil listing to index -- show active vigils with candle counts

--- web/app.py
+++ web/app.py
@@ -13,7 +13,11 @@ from flask import Flask, render_template, request, redirect, url_for
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 from candle.candle import create_candle
-from candle.store_sqlite import init_db, save_candle, get_active_candles, list_vigils
+from candle.vigil import create_vigil
+from candle.store_sqlite import (
+    init_db, save_vigil, get_vigil, list_vigils,
+    save_candle, get_active_candles,
+)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "digital-candle-secret"
@@ -21,31 +25,48 @@ app.config["SECRET_KEY"] = "digital-candle-secret"
 
 @app.route("/")
 def index():
-    """Show the main page."""
-    candles = get_active_candles("default")
-    return render_template("index.html", candles=candles)
+    """Show the main page with active vigils."""
+    vigils = list_vigils()
+    return render_template("index.html", vigils=vigils)
 
 
-@app.route("/light", methods=["POST"])
-def light():
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    """Create a new vigil."""
+    if request.method == "POST":
+        name = request.form.get("name", "").strip()
+        if not name:
+            return render_template("create.html", error="Please enter a name")
+        vigil = create_vigil(name)
+        save_vigil(vigil)
+        return redirect(url_for("vigil_page", vigil_id=vigil["id"]))
+    return render_template("create.html")
+
+
+@app.route("/vigil/<vigil_id>")
+def vigil_page(vigil_id):
+    """Show a vigil with its candles."""
+    vigil = get_vigil(vigil_id)
+    if not vigil:
+        return redirect(url_for("index"))
+    candles = get_active_candles(vigil_id)
+    return render_template("index.html", vigil=vigil, candles=candles)
+
+
+@app.route("/vigil/<vigil_id>/light", methods=["POST"])
+def light(vigil_id):
     """Light a new candle."""
     ip = request.remote_addr or "unknown"
-    candle = create_candle("default", ip)
+    candle = create_candle(vigil_id, ip)
     save_candle(candle)
-    return redirect(url_for("index"))
+    return redirect(url_for("vigil_page", vigil_id=vigil_id))
 
 
 def parse_args():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(description="Digital-Candle server")
-    parser.add_argument(
-        "--port", type=int, default=5000,
-        help="Port to run on (default: 5000)",
-    )
-    parser.add_argument(
-        "--debug", action="store_true",
-        help="Run in debug mode",
-    )
+    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument("--debug", action="store_true")
     return parser.parse_args()
 
 
