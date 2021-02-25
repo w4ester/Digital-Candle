b3fef9817dd9e18184ef9341ac79733952d2fdf4
Will F <11763006+w4ester@users.noreply.github.com>
2021-02-24 19:00:00 -0500

    add vigil themes -- memorial, celebration, solidarity, hope

--- candle/vigil.py
+++ candle/vigil.py
@@ -1,19 +1,53 @@
 """
 Vigil management.
 
-A vigil is a named collection of candles.
+A vigil is a named collection of candles. People gather around
+a vigil to light candles together. Each vigil has a theme.
 """
 
 import time
 import uuid
 
 
-def create_vigil(name):
+THEMES = {
+    "memorial": {
+        "name": "Memorial",
+        "description": "Remembering someone who has passed",
+        "color": "#c9a227",
+    },
+    "celebration": {
+        "name": "Celebration",
+        "description": "Celebrating a milestone or achievement",
+        "color": "#e8b931",
+    },
+    "solidarity": {
+        "name": "Solidarity",
+        "description": "Standing together through difficulty",
+        "color": "#d4a017",
+    },
+    "hope": {
+        "name": "Hope",
+        "description": "Looking forward to better days",
+        "color": "#f0c040",
+    },
+}
+
+
+def create_vigil(name, theme="solidarity"):
     """Create a new vigil."""
+    if theme not in THEMES:
+        theme = "solidarity"
+
     vigil = {
         "id": str(uuid.uuid4())[:8],
         "name": name,
+        "theme": theme,
         "created_at": time.time(),
         "candle_count": 0,
     }
     return vigil
+
+
+def get_themes():
+    """Return available vigil themes."""
+    return THEMES
