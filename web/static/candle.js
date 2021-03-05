0775f0d34b0e02b4e7308ac3768612e4e0a88d4e
Will F <11763006+w4ester@users.noreply.github.com>
2021-03-05 14:00:00 -0500

    add real-time candle light event -- everyone sees new candles instantly

--- web/static/candle.js
+++ web/static/candle.js
@@ -1,21 +1,54 @@
 /**
- * Digital-Candle client-side candle interactions.
- * Light button and basic animation triggers.
+ * Digital-Candle client-side SocketIO and candle interactions.
  */
 
-document.addEventListener("DOMContentLoaded", function() {
+var socket = null;
+var currentVigil = null;
+
+function initVigil(vigilId) {
+    currentVigil = vigilId;
+
+    socket = io();
+
+    socket.on("connect", function() {
+        console.log("Connected to server");
+    });
+
+    socket.on("candle_lit", function(data) {
+        addCandleToGrid(data);
+    });
+
     var lightBtn = document.getElementById("light-btn");
     if (lightBtn) {
         lightBtn.addEventListener("click", function() {
-            // For now, just submit the form
-            var vigilId = lightBtn.getAttribute("data-vigil");
-            if (vigilId) {
-                var form = document.createElement("form");
-                form.method = "POST";
-                form.action = "/vigil/" + vigilId + "/light";
-                document.body.appendChild(form);
-                form.submit();
-            }
+            socket.emit("light_candle", { vigil_id: vigilId });
         });
     }
-});
+}
+
+function addCandleToGrid(data) {
+    var grid = document.getElementById("candle-grid");
+    if (!grid) return;
+
+    var candle = document.createElement("div");
+    candle.className = "candle";
+    candle.setAttribute("data-id", data.candle_id);
+
+    var flame = document.createElement("div");
+    flame.className = "flame";
+    var flameInner = document.createElement("div");
+    flameInner.className = "flame-inner";
+    flame.appendChild(flameInner);
+
+    var wax = document.createElement("div");
+    wax.className = "wax";
+
+    candle.appendChild(flame);
+    candle.appendChild(wax);
+
+    if (grid.firstChild) {
+        grid.insertBefore(candle, grid.firstChild);
+    } else {
+        grid.appendChild(candle);
+    }
+}
