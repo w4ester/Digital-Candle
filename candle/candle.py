2cc8a656098267bd6f563f4b0e2a51feb0ec1fb9
Will F <11763006+w4ester@users.noreply.github.com>
2021-04-18 20:00:00 -0400

    add dedication messages on candles -- in memory of, in honor of, thinking of

--- candle/candle.py
+++ candle/candle.py
@@ -3,20 +3,24 @@ Candle state management.
 
 A candle has a lifecycle: created -> lit -> expired.
 Candles burn for a configurable duration (default 24 hours)
-and then fade out.
+and then fade out. Each candle can carry a dedication message.
 """
 
 import time
 import uuid
 
 
-def create_candle(vigil_id, ip_address, burn_hours=24):
-    """Create a new candle in a vigil."""
+def create_candle(vigil_id, ip_address, dedication="", burn_hours=24):
+    """Create a new candle in a vigil.
+
+    Returns the candle dict.
+    """
     candle = {
         "id": str(uuid.uuid4())[:8],
         "vigil_id": vigil_id,
         "lit_at": time.time(),
         "expires_at": time.time() + (burn_hours * 3600),
+        "dedication": dedication,
         "ip_address": ip_address,
         "active": True,
     }
@@ -38,3 +42,25 @@ def time_remaining(candle):
     """Get seconds remaining before candle expires."""
     remaining = candle["expires_at"] - time.time()
     return max(0, remaining)
+
+
+def format_dedication(dedication):
+    """Format a dedication message for display.
+
+    Supports prefixes: 'memory', 'honor', 'thinking'
+    """
+    if not dedication:
+        return ""
+
+    prefixes = {
+        "memory": "In memory of",
+        "honor": "In honor of",
+        "thinking": "Thinking of",
+    }
+
+    for key, prefix in prefixes.items():
+        if dedication.lower().startswith(key + ":"):
+            name = dedication[len(key) + 1:].strip()
+            return f"{prefix} {name}"
+
+    return dedication
