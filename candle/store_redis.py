9e76c528d41cf1d70aedb4560875f7d551451b47
Will F <11763006+w4ester@users.noreply.github.com>
2021-04-21 15:00:00 -0400

    add redis TTL for candle expiration -- no background cleanup needed

--- candle/store_redis.py
+++ candle/store_redis.py
@@ -1,8 +1,8 @@
 """
 Redis storage backend for Digital-Candle.
 
-Uses Redis hashes for vigils and candles.
-Same interface as store_sqlite.
+Uses Redis hashes for vigils and candles, sorted sets for
+ordering, and TTL for automatic candle expiration.
 """
 
 import json
@@ -43,8 +43,11 @@ def save_vigil(vigil):
         "id": vigil["id"],
         "name": vigil["name"],
         "theme": vigil.get("theme", "solidarity"),
+        "dedication": vigil.get("dedication", ""),
         "created_at": str(vigil["created_at"]),
         "candle_count": str(vigil.get("candle_count", 0)),
+        "peak_presence": str(vigil.get("peak_presence", 0)),
+        "total_candles_lit": str(vigil.get("total_candles_lit", 0)),
     })
     r.zadd("vigils:index", {vigil["id"]: vigil["created_at"]})
 
@@ -57,6 +60,8 @@ def get_vigil(vigil_id):
         return None
     data["created_at"] = float(data["created_at"])
     data["candle_count"] = int(data["candle_count"])
+    data["peak_presence"] = int(data.get("peak_presence", 0))
+    data["total_candles_lit"] = int(data.get("total_candles_lit", 0))
     return data
 
 
@@ -73,7 +78,7 @@ def list_vigils():
 
 
 def save_candle(candle):
-    """Save a candle as a Redis hash."""
+    """Save a candle as a Redis hash with TTL for expiration."""
     r = get_conn()
     key = f"candle:{candle['id']}"
     r.hset(key, mapping={
@@ -81,9 +86,16 @@ def save_candle(candle):
         "vigil_id": candle["vigil_id"],
         "lit_at": str(candle["lit_at"]),
         "expires_at": str(candle["expires_at"]),
+        "dedication": candle.get("dedication", ""),
         "ip_address": candle.get("ip_address", ""),
         "active": "1" if candle.get("active", True) else "0",
     })
+
+    # Set TTL so candle auto-expires -- no background cleanup needed
+    ttl_seconds = int(candle["expires_at"] - time.time())
+    if ttl_seconds > 0:
+        r.expire(key, ttl_seconds)
+
     vigil_key = f"vigil:{candle['vigil_id']}:candles"
     r.zadd(vigil_key, {candle["id"]: candle["lit_at"]})
 
@@ -95,17 +107,44 @@ def get_active_candles(vigil_id):
     candle_ids = r.zrevrange(vigil_key, 0, -1)
 
     candles = []
+    expired_ids = []
+
     for cid in candle_ids:
-        data = r.hgetall(f"candle:{cid}")
+        key = f"candle:{cid}"
+        data = r.hgetall(key)
+
         if not data:
+            expired_ids.append(cid)
             continue
+
         data["lit_at"] = float(data["lit_at"])
         data["expires_at"] = float(data["expires_at"])
         data["active"] = data["active"] == "1"
+
         if data["active"] and data["expires_at"] > time.time():
             candles.append(data)
+        else:
+            expired_ids.append(cid)
+
+    # Clean up expired references
+    if expired_ids:
+        r.zrem(vigil_key, *expired_ids)
 
     return candles
+
+
+def expire_candles(vigil_id):
+    """Check for and clean up expired candles. Returns expired IDs."""
+    r = get_conn()
+    vigil_key = f"vigil:{vigil_id}:candles"
+    candle_ids = r.zrange(vigil_key, 0, -1)
+    expired_ids = []
+    for cid in candle_ids:
+        if not r.exists(f"candle:{cid}"):
+            expired_ids.append(cid)
+    if expired_ids:
+        r.zrem(vigil_key, *expired_ids)
+    return expired_ids
 
 
 def publish_event(channel, event_data):
