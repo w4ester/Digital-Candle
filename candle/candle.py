21fc7a1e8bb20c90d7c287b7ea671c0140cde0fe
Will F <11763006+w4ester@users.noreply.github.com>
2021-04-30 20:00:00 -0400

    add rate limiting -- max 10 candles per minute per IP

--- candle/candle.py
+++ candle/candle.py
@@ -10,11 +10,24 @@ import time
 import uuid
 
 
+# Rate limiting: max candles per IP per minute
+RATE_LIMIT = 10
+RATE_WINDOW = 60  # seconds
+
+# Track lighting attempts by IP
+# { ip_address: [timestamp, timestamp, ...] }
+rate_tracker = {}
+
+
 def create_candle(vigil_id, ip_address, dedication="", burn_hours=24):
     """Create a new candle in a vigil.
 
-    Returns the candle dict.
+    Returns the candle dict or None if rate limited.
     """
+    # Check rate limit
+    if not check_rate_limit(ip_address):
+        return None
+
     candle = {
         "id": str(uuid.uuid4())[:8],
         "vigil_id": vigil_id,
@@ -24,7 +37,33 @@ def create_candle(vigil_id, ip_address, dedication="", burn_hours=24):
         "ip_address": ip_address,
         "active": True,
     }
+
     return candle
+
+
+def check_rate_limit(ip_address):
+    """Check if an IP address has exceeded the rate limit.
+
+    Returns True if the request is allowed, False if rate limited.
+    """
+    now = time.time()
+    window_start = now - RATE_WINDOW
+
+    # Clean old entries
+    if ip_address in rate_tracker:
+        rate_tracker[ip_address] = [
+            t for t in rate_tracker[ip_address] if t > window_start
+        ]
+    else:
+        rate_tracker[ip_address] = []
+
+    # Check limit
+    if len(rate_tracker[ip_address]) >= RATE_LIMIT:
+        return False
+
+    # Record this attempt
+    rate_tracker[ip_address].append(now)
+    return True
 
 
 def is_expired(candle):
@@ -58,6 +97,7 @@ def format_dedication(dedication):
         "thinking": "Thinking of",
     }
 
+    # Check if dedication starts with a known prefix key
     for key, prefix in prefixes.items():
         if dedication.lower().startswith(key + ":"):
             name = dedication[len(key) + 1:].strip()
